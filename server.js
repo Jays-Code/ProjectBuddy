270b97a28a0078f693a0797e2c8437d86f7fb253
Jays-Code <dfbfbc@gmail.com>
2019-05-14 21:05:55 -0400

    basic routes setup and working

--- server.js
+++ server.js
@@ -2,18 +2,42 @@ const express = require('express');
 const app = express();
 
 app.use(express.json());
-app.get('/', (req,res) => {
-  res.send('Hello world!')
-})
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
-  console.log("Got you connected on port" + PORT);
+    console.log("Got you connected on port" + PORT);
 })
 
 
-  app.use(express.static(__dirname + '/client/build/'));
+app.use(express.static(__dirname + '/client/build/'));
 
-  app.get('/', (req,res) => {
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/client/build/index.html')
-  })
+})
+
+
+
+app.get('/', (req, res) => {
+    res.send('Hello world!')
+})
+app.get('/startedProjects', (req, res) => {
+    res.send('Started Projects component page')
+});
+app.get('/startedProjects/:projId', (req, res) => {
+    res.send('Single Started Product component page')
+})
+
+app.get('/projectsToStart', (req, res) => {
+    res.send('Projects to Start component page')
+})
+
+app.get('/projectsToStart/:projId', (req, res) => {
+    res.send('Project to Start component page c')
+})
+
+/*
+
+<Route path='projectsToStart/:projId' component={ProjToStartComp} />
+*/
+
+  //ProjectsToStartComp page
