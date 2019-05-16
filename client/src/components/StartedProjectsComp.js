16f9090d011d31af47f006a81e3c50c26a932e10
Jays-Code <dfbfbc@gmail.com>
2019-05-16 16:42:02 -0400

    wrote code for axios post request

--- client/src/components/StartedProjectsComp.js
+++ client/src/components/StartedProjectsComp.js
@@ -28,10 +28,10 @@ class StartedProjectsComp extends Component {
     createStartedProject = (e) => {
         e.preventDefault()
         axios.post('/api/startedProjects', {
-                name: this.state.newStartedProject.name,
-                rank: this.state.newStartedProject.rank,
-                info: this.state.newCreature.info
-            })
+            name: this.state.newStartedProject.name,
+            rank: this.state.newStartedProject.rank,
+            info: this.state.newCreature.info
+        })
             .then(res => {
                 const startedProjectsList = [...this.state.startedProjects]
                 startedProjectsList.unshift(res.data)
@@ -44,9 +44,9 @@ class StartedProjectsComp extends Component {
                     startedProjects: startedProjectsList
                 })
             })
-    
-      }
-    
+    }
+
+
     //<form onSubmit = {this.createStartedProject}></form>
 
 
@@ -55,10 +55,55 @@ class StartedProjectsComp extends Component {
             <div>
                 <h1>Started Projects</h1>
                 <Link to="/">Go Back to Home</Link>
-
+                <form onSubmit={this.createStartedProject}>
+    <div>
+        <label htmlFor="name">Name</label>
+        <input
+            id="name"
+            type="text"
+            name="name"
+            onChange={this.handleChange}
+            value={this.state.newStartedProject.name}
+        />
+    </div>
+    <div>
+        <label htmlFor="rank">Rank</label>
+        <textarea
+            id="description"
+            type="number"
+            name="description"
+            onChange={this.handleChange}
+            value={this.state.newStartedProject.rank}
+        />
+    </div>
+    <div>
+        <label htmlFor="rank">Info</label>
+        <textarea
+            id="info"
+            type="number"
+            name="info"
+            onChange={this.handleChange}
+            value={this.state.newStartedProject.rank}
+        />
+    </div>
+    <button>Add a previously started project</button>
+</form>
             </div>
         )
     }
 }
 
 export default StartedProjectsComp
+
+////////////
+{
+    this.state.startedProjects.map(startedProject => {
+        return (
+            <div key={startedProject.projId}>
+                <Link to={`/${startedProject.projId}`}>{startedProject.name}</Link>
+            </div>
+        )
+    })
+}
+
+  
