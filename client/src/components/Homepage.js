be57c3ae047a92599f875f516b5c21d74969d42f
Jays-Code <dfbfbc@gmail.com>
2019-05-16 14:01:49 -0400

    working on homepage components page

--- client/src/components/Homepage.js
+++ client/src/components/Homepage.js
@@ -2,14 +2,26 @@ import React, { Component } from 'react';
 import { Redirect, Link } from 'react-router-dom'
 
 class Homepage extends Component {
+    /*
+        state = { isProjectStarted: false };
+    
+        toggleIsProjectStarted = () => {
+            let isProjectStarted = !this.state.isProjectStarted;
+            this.setState({ isProjectStarted });
+            //<button onClick={this.toggleIsProjectStarted}></button> --- this line was under h1 tag
+            //had originally attempted to use button toggles, but can just switch to new component with Link
+        }
+    */
+
     render() {
         return (
             <div>
-                <h1>This is the Home Page</h1>
-                
+                <h1>Welcome to ProjectBuddy!</h1>
+                <Link to="/StartedProjectsComp">Started Projects</Link>
+                <Link to="/ProjectsToStartComp">Projects To Start</Link>
             </div>
-        )
-    }
-}
-
+                )
+            }
+        }
+        
 export default Homepage
