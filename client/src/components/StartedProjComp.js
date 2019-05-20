a5e3f19d6d1ebedccf1cd0b7dfbed7b1a1970418
Jays-Code <dfbfbc@gmail.com>
2019-05-20 09:50:36 -0400

    working on update for startedProjects. Form currently not allowing edits. Troubleshooting

--- client/src/components/StartedProjComp.js
+++ client/src/components/StartedProjComp.js
@@ -19,11 +19,20 @@ class StartedProjComp extends Component {
 
     deleteProject = () => {
         axios.delete(`/api/startedProjects/${this.props.match.params.projId}`).then(<Redirect to="/api/startedProjects" />)
+    }
+
+    updateStartedProject = () => {
+        axios.put(`/api/startedProjects/${this.props.match.params.projId}`, {
+            name: this.state.startedProject.name,
+            rank: this.state.startedProject.rank,
+            info: this.state.startedProject.info
+        }).then(res => {
+            this.setState({ startedProject: res.data })
+        })
 
     }
 
     render() {
-
         return (
             <div>
                 <h1>{this.state.startedProject.name}</h1>
@@ -32,8 +41,45 @@ class StartedProjComp extends Component {
                 <div>
                     <button onClick={this.deleteProject}>Delete this project</button>
                 </div>
+                <div>
+                    <form onSubmit={this.updateStartedProject}>
+                        <div>
+                            <label htmlFor="name">Name</label>
+                            <input
+                                id="name"
+                                type="text"
+                                name="name"
+                                onChange={this.handleChange}
+                                value={this.state.startedProject.name}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="rank">Rank</label>
+                            <input
+                                id="rank"
+                                type="text"
+                                name="rank"
+                                onChange={this.handleChange}
+                                value={this.state.startedProject.rank}
+                            />
+                        </div>
+                        <div>
+                            <label htmlFor="info">Info</label>
+                            <textarea
+                                id="dinfo"
+                                name="info"
+                                onChange={this.handleChange}
+                                value={this.state.startedProject.info}
+                            />
+                        </div>
+                        <button>Update</button>
+                    </form>
+                </div>
             </div>
+
+
         )
+
     }
 
 
